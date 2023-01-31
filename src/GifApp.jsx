821d15bdd1329f26ac99c5d26b4919e692af3015
Bryan Parada <bryan.parada.cea@gmail.com>
2023-01-31 11:00:35 -0300

    RS6 - Creating a category list

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+//     ^^ esta importacion es opcional, ya viene por defecto
 
 export const GifApp = () => {
+
+  const [categories, setCategories] = useState(['one punch', 'dbz'])
+
+  console.log(categories)
+
+
   return (
    <>
-   <h1> hola </h1>
-   
+          {/* titulo */}
+          <h1> hola </h1>
+          
+          {/* input */}
+
+      {/* listado de gif */}
+    <ol>
+     { categories.map( category => {
+      return <li key={category}>{category}</li>
+     }) }
+      <li>ABC</li>
+    </ol>
+
+      {/* gif item */}
+
    </>
   )
 }
